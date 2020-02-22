313e8496b07c5b2b26370a70b4608c1b8efa0247
Dima Pashkalov <paskalovdima44@gmail.com>
2020-02-22 19:32:17 +0700

    Initial commit

---
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -0,0 +1,24 @@
+package com.mygdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		config.useAccelerometer = false;
+		config.useCompass = false;
+
+		initialize(new MyGdxGame(), config);
+	}
+}
